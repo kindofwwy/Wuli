127da5b3c5b63a9d35816319f431d073d7f56261
Weiwy <weiwuyu2005@163.com>
2022-11-02 03:00:03 +0800

    pid

--- wulitest/wulitest29.py
+++ wulitest/wulitest29.py
@@ -15,18 +15,19 @@ y1=0
 lei=0
 a=Phy(10,[0,0,0],[0,0,0],r=10)
 k1=0.5
-k2=-2
+k2=3
 k3=0.0008
 b=Phy.tgraph()
 c=Phy.tgraph()
 d=Phy.tgraph()
+pcha=0
 while True:
 
     cha=y1-a.p[1]
-    v=a.v[1]
+    v=(cha-pcha)/t
     lei+=cha
-    print(lei*k3)
     a.force([0,k1*cha +k2*v +k3*lei,0])
+    pcha=cha
 
     a.force([0,-10,0])
     Phy.run(t)
@@ -34,7 +35,7 @@ while True:
     turtle.onscreenclick(rey)
     turtle.goto(0,y1)
     turtle.dot(30,"red")
-    b.draw(None,k1*cha,[100,0],color="red",tiao=80,kx=10,ky=3,bi=True,chang=50)
-    c.draw(None, k2*v, [100, 0], color="blue", tiao=80, kx=10,ky=3, bi=True,chang=50)
-    d.draw(None, k3*lei, [100, 0], color="green", tiao=80, kx=10,ky=3, bi=True,chang=50)
+    b.draw(None,k1*cha,[100,0],color="red",tiao=50,kx=10,ky=3,bi=True,chang=20)
+    c.draw(None, k2*v, [100, 0], color="blue", tiao=50, kx=10,ky=3, bi=True,chang=20)
+    d.draw(None, k3*lei, [100, 0], color="green", tiao=50, kx=10,ky=3, bi=True,chang=20)
     turtle.listen()
