16bb251b590d004a10efc69bb44bedda9ed62254
Weiwy <weiwuyu2005@163.com>
2022-11-16 00:11:54 +0800

    修复bug

--- wulitest/wulitest26.py
+++ wulitest/wulitest26.py
@@ -6,10 +6,11 @@ b=Phy(5,[0,0,0],[0,-100,0],r=5,color="red")
 s=[]
 for i in range(10000):
     a.resilience(100,other=b)
-    s.append(Phy.saveone())
+    s.append(str(Phy.saveone()))
     Phy.run(0.01)
 
 for i in s:
-    Phy.readone(i)
+    Phy.readone(eval(i))
     Phy.tplay()
     time.sleep(0.03)
+
